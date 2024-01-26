12a26501d563740b0a23de4ee8188cb79ed749fe
23samantabaez <23sbaezgomez@gmail.com>
2024-01-26 00:34:18 -0500

    Updated page

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,3 +4,12 @@ const copyrightYear = new Date();
 currentYear.innerText = copyrightYear.getFullYear();
 
 document.getElementById("lastModified").innerHTML = new Date(document.lastModified);
+
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
