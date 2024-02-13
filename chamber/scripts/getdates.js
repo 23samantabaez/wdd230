947ba9a1fbc606e0c0d3a144037b3528aee24806
23samantabaez <23sbaezgomez@gmail.com>
2024-02-13 13:15:27 -0500

    .

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -5,7 +5,7 @@ currentYear.innerText = copyrightYear.getFullYear();
 
 document.getElementById("lastModified").innerHTML = new Date(document.lastModified);
 
-
+/*menu */
 const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#menu');
 
@@ -13,3 +13,22 @@ hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('show');
     hambutton.classList.toggle('show');
 });
+
+
+/*dark mode*/
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
+
+
