1df6e9c3a513d4b5e4274f350a261ceba80b7caf
23samantabaez <23sbaezgomez@gmail.com>
2024-03-11 15:02:04 -0400

    .

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -8,11 +8,13 @@ fetch("data/members.json")
     let out = "";
     for (let company of companies){
         out += `
-        <h2>${company.name}</h2>
-        <p><strong>Address:</strong> ${company.address}</p>
-        <p><strong>Phone:</strong> ${company.phone}</p>
-        <p><strong>Website:</strong> <a href="${company.website}" target="_blank">${company.website}</a></p>
-        <img src="${company.logo}" alt="${company.name} Logo" width="100" height="100">
+        <div class="company-card">
+                <h2>${company.name}</h2>
+                <p><strong>Address:</strong> ${company.address}</p>
+                <p><strong>Phone:</strong> ${company.phone}</p>
+                <p><strong>Website:</strong> <a href="${company.website}" target="_blank">${company.website}</a></p>
+                <img src="${company.logo}" alt="${company.name} Logo" width="100" height="100">
+        </div>
         `;
     }
     placeholder.innerHTML = out;
